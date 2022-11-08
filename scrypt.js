464aac326372492ca8e3853ae911c1b8991a1457
giogua03 <giorgiogua03@gmail.com>
2022-11-08 18:04:29 +0100

    finito

--- scrypt.js
+++ scrypt.js
@@ -1,15 +1,74 @@
-let arrNumeriRandom = []
+let nRandoms = 5;
+let minRandom = 1;
+let maxRandom = 100;
+let timeToHide = 1 * 7000;
 
+const eleNumbers = document.querySelector('.numbers');
 
+let arrRandoms = getArrRandomIntegers(nRandoms, minRandom, maxRandom);
+console.log(arrRandoms);
 
-function caso(min,max) {
-    return Math.floor(Math.random() * (max-min +1))
+for (let i = 0; i < arrRandoms.length; i++) {
+	eleNumbers.innerHTML += `<div class="number">${arrRandoms[i]}</div>`;
 }
-for ( let i = 0; i <= 5; i++ ){
-    let numeriRandom
-    numeriRandom = caso(1,99)
-    numeriRandom.push(arrNumeriRandom)
+
+setTimeout(hideNumbers, timeToHide);
+setTimeout(finishGame, timeToHide + 100);
+
+function hideNumbers() {
+	eleNumbers.innerHTML = '';
 }
-console.log[arrNumeriRandom]
-    
-    
+
+function finishGame() {
+	const userNumbers = askNumbers();
+	console.log(userNumbers);
+	const guessedIndexes = verifyNumbers(arrRandoms, userNumbers);
+	console.log(guessedIndexes);
+	sayScore(arrRandoms, guessedIndexes);
+}
+
+function askNumbers() {
+	const inputUser = prompt('Dammi i numeri separati da spazio: '); 
+	const arrInputUser = inputUser.split(' ');
+	
+	for (let i = 0; i < arrInputUser.length; i++) {
+		arrInputUser[i] = parseInt(arrInputUser[i]);
+	}
+	return arrInputUser;
+}
+
+function verifyNumbers(arr1, arr2) {
+	const arrGuessedIndexes = [];
+	for (let i = 0; i < arr1.length; i++) {
+		
+		if (arr1[i] == arr2[i]) {
+			arrGuessedIndexes.push(i);
+		}
+	}
+	return arrGuessedIndexes;
+}
+
+function sayScore(arrValues, arrIndexes) {
+	let msg;
+	if (arrIndexes.length === 0) {
+		msg = 'loser';
+	} else if (arrIndexes.length === arrValues.length) {
+		msg = 'hai vinto ';
+	} else {
+		msg = `hai indovinato ${arrIndexes.length}`;
+	}
+	console.log(msg);
+	alert(msg);
+}
+
+function getArrRandomIntegers(n, min, max) {
+	const arrRandoms = [];
+	for (let i = 0; i < n; i++) {
+		arrRandoms.push(getRandomInteger(min, max))
+	}
+	return arrRandoms;
+}
+
+function getRandomInteger(min, max) {
+	return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
