e9e36f36ed649c718495957b44a9975d7baf2961
giogua03 <giorgiogua03@gmail.com>
2022-11-08 16:59:00 +0100

    start

---
+++ scrypt.js
@@ -0,0 +1,15 @@
+let arrNumeriRandom = []
+
+
+
+function caso(min,max) {
+    return Math.floor(Math.random() * (max-min +1))
+}
+for ( let i = 0; i <= 5; i++ ){
+    let numeriRandom
+    numeriRandom = caso(1,99)
+    numeriRandom.push(arrNumeriRandom)
+}
+console.log[arrNumeriRandom]
+    
+    
